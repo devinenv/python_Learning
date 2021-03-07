7722ada0fd12e286107a6ec6eb867b3a0a985d46
devinenv <80189537+devinenv@users.noreply.github.com>
2021-03-07 11:32:01 +0530

    multiplication Table for any number

---
+++ multiplication.py
@@ -0,0 +1,6 @@
+#num = 17
+
+num = int(input("Display multiplication table of: "))
+# Iterate 10 times from i = 1 to 10(if you want to print ytable for 10 times)
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
